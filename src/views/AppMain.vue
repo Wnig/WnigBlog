bdf4d989c299e1289cb2800f4182f2fae243e37e
Wnig <Wnig@wnigdeMacBook-Pro.local>
2020-06-18 11:13:16 +0800

    路由修改

---
+++ src/views/AppMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="app-main">
+    <transition name="fade"
+                mode="out-in">
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"
+                     :key="$route.fullpath" />
+      </keep-alive>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'AppMain'
+}
+</script>
